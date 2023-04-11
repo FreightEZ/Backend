15e2427db96bf212401ff1dbeffc4ffb721a3f09
Rohan Sharma <therohansharma03@gmail.com>
2023-04-11 23:29:34 +0530

    version 2.0 of freightEZ Server

--- app.js
+++ app.js
@@ -1,23 +1,119 @@
-require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const RegisteredUser = require("./models/registeredUser");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+bodyParser = require("body-parser");
+const url =
+  "mongodb+srv://therohansharma3:Gq0rJmKip6wV2RYn@dev.vbq2sov.mongodb.net/FreightEZ?retryWrites=true&w=majority";
 
-var sha256 = require("js-sha256"); // For Encryption
-const mongoose = require("mongoose"); // For Database
+// salt to make things more secure
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecure = "kjqnfburr3iqefbewifbw";
+// use to parse json
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+// // parse application/json
+// app.use(bodyParser.json());
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
-const DB =
-  "mongodb+srv://therohansharma3:<password>@dev.vbq2sov.mongodb.net/test";
+// app.use(
+//   cors({
+//     // credentials: true,
+//     // app can able to communicate with it (client) i.e what kind of app can commnicate with our a.
+//     origin: "*",
+//   })
+// );
 
 mongoose
-  .connect(DB)
+  .connect(url)
   .then(() => {
-    console.log("connection successful");
+    console.log("Connection Successful");
   })
   .catch((err) => console.log("no connection"));
 
-const app = express();
-app.use(cors());
-let items;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const con = mongoose.connection;
+con.on("open", () => {
+  console.log("connected...");
+});
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  console.log("POST /register");
+  console.log(req.body);
+  const {
+    fullName,
+    companyName,
+    email,
+    phoneNumber,
+    companyAddress,
+    password,
+  } = req.body; // Update destructuring assignment
+
+  // try catch use to avoid duplicate email address.
+  try {
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt); // Hash the password
+    const registeredUser = new RegisteredUser({
+      fullName,
+      companyName,
+      email,
+      phoneNumber,
+      companyAddress,
+      password: hashedPassword, // Set hashed password
+    });
+
+    const savedUser = await registeredUser.save(); // Save the user to the database
+    console.log(savedUser);
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(422).json(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    console.log("POST /login");
+    console.log(req.body);
+    const { email, password } = req.body;
+    const registeredUser = await RegisteredUser.findOne({ email });
+    if (registeredUser) {
+      const passwordOk = bcrypt.compareSync(password, registeredUser.password);
+      if (passwordOk) {
+        jwt.sign(
+          { email: registeredUser.email, id: registeredUser._id },
+          jwtSecure,
+          {},
+          (err, token) => {
+            if (err) {
+              throw err;
+            } else {
+              res.cookie("token", token).json("pass ok");
+            }
+          }
+        );
+      } else {
+        res.status(422).json("Login failed, password not matched");
+      }
+    } else {
+      // throw err;
+      res.json("user not found");
+    }
+  } catch (error) {
+    console.log(error, error.message);
+  }
+});
+app.listen(4000, () => {
+  console.log("Server started at port 4000");
+});
---
+++ models/registeredUser.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Schema is a structure of the documents(data).(i.e meta data)
+const registeredUserSchema = new Schema({
+  fullName: String,
+  companyName: String,
+  email: { type: String, unique: true },
+  phoneNumber: { type: String, unique: true },
+  companyAddress: String,
+  password: String,
+});
+
+// Model is a class with which we construct documents. CURD operations beacome easy to perform
+// Collection creation is done by MODEL
+const RegistredUserModel = mongoose.model(
+  "RegistredUser", // Deafult "s" is added to the name of colletion
+  registeredUserSchema //Struture of added document
+);
+
+module.exports = RegistredUserModel;
